d35b7a4e9d2f5f0db80169c71f74f473cf3ca79d
Jitender sharma <122668928+jitender294@users.noreply.github.com>
2023-05-13 14:15:27 +0530

    Add files via upload

---
+++ bulb on-off/coding/index.js
@@ -0,0 +1,10 @@
+function bulbOn() {
+    let bulbobj = document.querySelector(".bulboff")
+    bulbobj.setAttribute("src", "Wh.jpeg")
+}
+
+function bulbOff() {
+    let bulbobj = document.querySelector(".bulboff")
+    bulbobj.setAttribute("src", "WhatsAp_1_287x531.jpeg")
+    bulbobj.setAttribute("src", "WhatsAp_1_287x531.jpeg")
+}
